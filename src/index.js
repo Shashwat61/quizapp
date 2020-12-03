2e9a40db8d5a662864c9ae38ba57e2132ebca134
Shashwat61 <shashwat1461.cse18@chitkara.edu.in>
2020-12-03 15:08:29 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './KnowledgeHut_QuizBee_Assets/assets/style.css';
+import quizService from './KnowledgeHut_QuizBee_Assets/quizService';
+import QuestionBox from './KnowledgeHut_QuizBee_Assets/components/QuestionBox.js';
+import Result from './KnowledgeHut_QuizBee_Assets/components/Result';
+class QuizBee extends React.Component{
+state={
+  questionBank:[],
+  score:0,
+  responses:0
+}
+
+getQuestions=()=>{
+  quizService().then(question =>{
+    this.setState({
+      questionBank:question
+    })
+  })
+}
+computeAnswer=(answer,correctAnswer)=>{
+if(answer===correctAnswer){
+  this.setState({
+    score:this.state.score + 1
+  })
+}
+
+this.setState({
+  responses:this.state.responses < 5 ? this.state.responses + 1 : 5
+})
+}
+playAgain=()=>{
+  this.getQuestions();
+  this.setState({
+    score:0,
+    responses:0
+  })
+}
+componentDidMount(){
+  this.getQuestions();
+}
+
+  render(){
+    return (
+      <div className="container">
+      <div className="title">helloth</div>
+      {this.state.questionBank.length>0 &&
+         this.state.responses<5 &&
+         this.state.questionBank.map(({question,answers,correct,questionId})=>
+      (<QuestionBox
+         question={question}
+         options={answers}
+          key={questionId}
+          selected={answer =>this.computeAnswer(answer,correct)}
+
+          />
+      )
+    )}
+    {this.state.responses===5 ? (<Result score={this.state.score} playAgain={this.playAgain}/> ) : null}
+      </div>
+
+    );
+  }
+}
+
+ReactDOM.render(<QuizBee/>,document.getElementById('root'));
